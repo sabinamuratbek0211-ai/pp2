37de07887b58cb88a56e732671f2b73bbb57ff99
Sabina <sabinamuratbek0211@gmail.com>
2026-01-30 01:23:58 +0500

    Initial_commit

--- practice1/python_basics/data-types.py
+++ practice1/python_basics/data-types.py
@@ -1,14 +1,31 @@
+#1
 x = 5
 print(type(x))
 
-#x = 20.5
-#display x:
-#print(x)
-#display the data type of x:
-#print(type(x)) 
+#2
+x = 20.5
+display x:
+print(x)
+display the data type of x:
+print(type(x)) 
 
-#x = tuple(("apple", "banana", "cherry"))
+#3
+x = tuple(("apple", "banana", "cherry"))
 #display x:
-#print(x)
+print(x)
 #display the data type of x:
-#print(type(x)) 
+print(type(x)) 
+
+#4
+x = range(10)
+#display x:
+print(x)
+#convert to list to display the content of x:
+print(list(x))
+
+#5
+x = True
+#display x:
+print(x)
+#display the data type of x:
+print(type(x)) 
